42ee6ac5bcae6d86f3ed9aa8496b882a3e0f25ac
sovkutsan <asmprogr@gmail.com>
2018-02-14 14:03:18 +0600

    some files 3

---
+++ app/AppFixtures.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sovkutsan
+ * Date: 2/14/18
+ * Time: 1:59 PM
+ */
+
+namespace App\DataFixtures;
+
+use AppBundle\Entity\Product;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        // create 20 products! Bam!
+        for ($i = 0; $i < 20; $i++) {
+            $product = new Product();
+            $product->setName('product '.$i);
+            $product->setPrice(mt_rand(10, 100));
+            $manager->persist($product);
+        }
+
+        $manager->flush();
+    }
+}
